ac3d90ca4f1e3770f4b5c873b5d9fbb094c22553
Metapsyche <mindrogen@gmail.com>
2022-05-03 16:31:18 -0400

    initial hangar setup

--- src/components/onepager/Splash.js
+++ src/components/onepager/Splash.js
@@ -5,6 +5,7 @@ function Splash() {
     <div className="content" style={{ padding: "12% 2%" }}>
       <div style={{ textAlign: "center" }}>
         <p className="mainfont" style={{ color: "whitesmoke", textShadow: '3px 3px gray', fontSize: '12vw' }}>Spacerbots</p>
+        <p className="mainfont" style={{ color: "whitesmoke", textShadow: '3px 3px gray', fontSize: '8vw' }}>Hangar</p>
       </div>
     </div>
   );
